87a011ab5d576fa83ddcabd17ae2e4af7f4219c8
ARTCat77 <art.division210@gmail.com>
2020-05-27 01:01:14 +0600

    programming JS

--- netclicks/js/main.js
+++ netclicks/js/main.js
@@ -1,8 +1,53 @@
+
 // Установка констант
 
 const leftMenu = document.querySelector('.left-menu');
 const menuHamburger = document.querySelector('.hamburger');
 const tvCard = document.querySelectorAll('.tv-card__img');
+const tvShowsList = document.querySelector('.tv-shows__list');
+const modal = document.querySelector('.modal');
+
+// Класс для создания карточек
+const DBService = class {
+    getData = async (url) => {
+        const res = await fetch(url);
+        if (res.ok) {
+            return res.json();
+        } else {
+            throw new Error(`Не удалось получить данные по адресу ${url}`);
+        }
+    }
+    getTestData = () => {
+        return this.getData('test.json');
+    }
+}
+
+const renderCard = response => {
+    console.log(response);
+    tvShowsList.textContent = '';
+
+    response.results.forEach(item => {
+
+        const card = document.createElement('li');
+        card.className = 'tv-shows__item';
+        card.innerHTML = `
+        <a href="#" class="tv-card">
+        <span class="tv-card__vote">7.4</span>
+        <img class="tv-card__img"
+            src="https://image.tmdb.org/t/p/w185_and_h278_bestv2/fhV9ckyBko1ZejEEmwdiXG8YMy5.jpg"
+            data-backdrop="https://image.tmdb.org/t/p/w185_and_h278_bestv2/AdJgkXb8oLI8e4rsk8XzkvABIuw.jpg"
+            alt="Звёздные войны: Повстанцы">
+        <h4 class="tv-card__head">Звёздные войны: Повстанцы</h4>
+        </a>
+        `;
+        tvShowsList.append(card);
+
+        console.log(card);
+    })
+}
+
+new DBService().getTestData().then(renderCard);
+// ! Класс для создания карточек
 
 // Меню
 // Открытие <-> закрытие меню
@@ -31,7 +76,7 @@ leftMenu.addEventListener('click', event => {
     }
 })
 
-//Смена картинки на карточке при наведении
+// Смена картинки на карточке при наведении
 for (let i = 0; i < tvCard.length; i++) {
     let imgSrc = tvCard[i].src;
     let imgChng = tvCard[i].getAttribute('data-backdrop');
@@ -41,7 +86,24 @@ for (let i = 0; i < tvCard.length; i++) {
     tvCard[i].addEventListener('mouseout', event => {
         tvCard[i].src = imgSrc;
     });
-}
+} // ! Смена картинки на карточке при наведении
 
+// Модальное окно
+tvShowsList.addEventListener('click', event => {
+    const target = event.target;
+    const card = target.closest('.tv-card');
 
+    if (card) {
+        document.body.style.overflow = 'hidden';
+        modal.classList.remove('hide');
+    }
+
+})
+
+modal.addEventListener('click', event => {
+    if (event.target.closest('.cross') || event.target.classList.contains('modal')) {
+        document.body.style.overflow = '';
+        modal.classList.add('hide');
+    }
+}) // ! Модальное окно
 
