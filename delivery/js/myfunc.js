ef8c1a7643ce78077f7c5cafc1015e1b020ba985
ARTCat77 <art.division210@gmail.com>
2020-05-26 21:34:08 +0600

    Добавлены JS

--- delivery/js/myfunc.js
+++ delivery/js/myfunc.js
@@ -1,24 +1,12 @@
-// находим все фреймы в документе
-let iframes = document.querySelectorAll("iframe");
+const btnCart = document.querySelector('#cart-button');
+const modal = document.querySelector('.modal');
+const btnClose = document.querySelector('.close');
+const btnCancel = document.querySelector('#close');
 
-// перебираем массив (на самом деле здесь мы имеем дело с объектом Nodelist, но это неважно)
-for (let i = 0; i < iframes.length; i++) {
-  let iframe = iframes[i],
-    // сохраняем оригинальное значение srс
-    originalSrc = iframe.src;
+btnCart.addEventListener('click', toggleModal);
+btnCancel.addEventListener('click', toggleModal);
+btnClose.addEventListener('click', toggleModal);
 
-  // заменяем содержимое фрейма картинкой
-  iframe.src =
-    "https://fromwebwithlove.000webhostapp.com/media/iframe-cover.jpg";
-
-  // при наведении курсора отображаем содержимое фрейма
-  iframe.addEventListener("mouseover", () => (iframe.src = originalSrc));
-
-  // возвращаем "кавер"
-  iframe.addEventListener(
-    "mouseout",
-    () =>
-      (iframe.src =
-        "https://fromwebwithlove.000webhostapp.com/media/iframe-cover.jpg")
-  );
+function toggleModal() {
+  modal.classList.toggle('is-open');
 }
