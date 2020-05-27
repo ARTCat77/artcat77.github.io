67e0f6771bd9f4ec98e60634a67320f928324ea8
ARTCat77 <art.division210@gmail.com>
2020-05-27 13:07:40 +0600

    Begin JS исправления по макету

--- delivery/js/myfunc.js
+++ delivery/js/myfunc.js
@@ -1,7 +1,17 @@
-const btnCart = document.querySelector('#cart-button');
-const modal = document.querySelector('.modal');
-const btnClose = document.querySelector('.close');
-const btnCancel = document.querySelector('#close');
+const btnCart = document.querySelector('#cart-button'),
+  modal = document.querySelector('.modal'),
+  btnClose = document.querySelector('.close'),
+  btnCloseAuth = document.querySelector('.close-auth'),
+  modalAuth = document.querySelector('.modal-auth'),
+  btnAuth = document.querySelector('.button-auth'),
+  btnOut = document.querySelector('.button-out'),
+  loginForm = document.querySelector('#logInForm'),
+  loginInput = document.querySelector('#login'),
+  userName = document.querySelector('.user-name'),
+
+  btnCancel = document.querySelector('#close');
+
+let login = localStorage.getItem('deliveryJS');
 
 btnCart.addEventListener('click', toggleModal);
 btnCancel.addEventListener('click', toggleModal);
@@ -12,3 +22,63 @@ function toggleModal() {
 }
 
 new WOW().init();
+
+// Day 1
+
+function toggleModalAuth() {
+  modalAuth.classList.toggle('is-open');
+}
+
+function checkAuth() {
+  if (login) {
+    authorized();
+  } else {
+    notAuthorized();
+  }
+}
+checkAuth();
+
+function authorized() {
+
+  function logOut(event) {
+    login = null;
+    localStorage.removeItem('deliveryJS');
+    btnAuth.style.display = 'block';
+    // btnCart.style.display = 'initial';
+    btnOut.style.display = '';
+    userName.style.display = '';
+    userName.textContent = login;
+    btnOut.removeEventListener('click', logOut);
+
+    checkAuth();
+  }
+
+  btnAuth.style.display = 'none';
+  // btnCart.style.display = 'initial';
+  btnOut.style.display = 'initial';
+  userName.style.display = 'inline';
+  userName.textContent = login;
+
+  btnOut.addEventListener('click', logOut);
+}
+
+function notAuthorized() {
+
+  function logIn(event) {
+    event.preventDefault();
+    login = loginInput.value;
+
+    localStorage.setItem('deliveryJS', login);
+
+    toggleModalAuth();
+    btnAuth.removeEventListener('click', toggleModalAuth);
+    btnCloseAuth.removeEventListener('click', toggleModalAuth);
+    loginForm.removeEventListener('submit', logIn);
+    loginForm.reset();
+    checkAuth();
+  }
+
+  btnAuth.addEventListener('click', toggleModalAuth);
+  btnCloseAuth.addEventListener('click', toggleModalAuth);
+  loginForm.addEventListener('submit', logIn);
+}
