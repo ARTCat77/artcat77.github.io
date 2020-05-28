bfbf8c04a5ee9f3bd49352e4c84e9f4bcd3bb1ab
ARTCat77 <art.division210@gmail.com>
2020-05-28 07:56:54 +0600

    Изменения

--- delivery/js/myfunc.js
+++ delivery/js/myfunc.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const btnCart = document.querySelector('#cart-button'),
   modal = document.querySelector('.modal'),
   btnClose = document.querySelector('.close'),
@@ -8,20 +10,32 @@ const btnCart = document.querySelector('#cart-button'),
   loginForm = document.querySelector('#logInForm'),
   loginInput = document.querySelector('#login'),
   userName = document.querySelector('.user-name'),
-
+  contPromo = document.querySelector('.container-promo'),
+  contRestaurants = document.querySelector('.restaurants'),
+  cardsRestaurants = document.querySelector('.cards-restaurants'),
+  menu = document.querySelector('.menu'),
+  logo = document.querySelector('.logo'),
+  cardsMenu = document.querySelector('.cards-menu'),
   btnCancel = document.querySelector('#close');
 
 let login = localStorage.getItem('deliveryJS');
 
-btnCart.addEventListener('click', toggleModal);
-btnCancel.addEventListener('click', toggleModal);
-btnClose.addEventListener('click', toggleModal);
+const getData = async function (url) {
+  const responce = await fetch(url);
+  console.log(responce.json);
+
+  if (responce.ok) {
+    return responce.json();
+  } else {
+    throw new Error(`Не удалось получить данные по адресу ${url}, статус: ошибка ${responce.status}`);
+  }
+};
+
+getData('db/partners.json');
 
 function toggleModal() {
   modal.classList.toggle('is-open');
 }
-
-new WOW().init();
 
 // Day 1
 
@@ -82,3 +96,104 @@ function notAuthorized() {
   btnCloseAuth.addEventListener('click', toggleModalAuth);
   loginForm.addEventListener('submit', logIn);
 }
+
+// Day 2
+
+function createCard() {
+
+  const card = `
+          <a class="card card-restaurant wow fadeInLeft" data-wow-duration="2s"
+            data-wow-delay="0.4s">
+            <img src="img/catalog/c1.jpg" alt="" class="card-image">
+            <div class="card-text">
+              <div class="card-heading">
+                <h3 class="card-title">Пицца плюс</h3>
+                <span class="card-tag tag">50 мин.</span>
+              </div>
+              <div class="card-info">
+                <div class="rating">
+                  <img src="img/rating.svg" alt="rating" class="rating-pic">
+                  4.5
+                </div>
+                <div class="price">От 900 ₸</div>
+                <div class="category">Пицца</div>
+              </div>
+            </div>
+          </a>
+  `;
+
+  cardsRestaurants.insertAdjacentHTML('beforeend', card);
+}
+
+function createMenuCard() {
+  const card = document.createElement('div');
+  card.className = 'card';
+
+  card.insertAdjacentHTML('beforeend', `
+            <img src="img/pizza-plus/pizza-classic.jpg" alt="Пицца Классика"    class="card-image"> 
+            <div class="card-text">
+              <div class="card-heading">
+                <h3 class="card-title">Пицца Классика</h3>
+              </div>
+              <div class="card-info">
+                <div class="ingredients">
+                  Соус томатный, сыр «Моцарелла», сыр «Пармезан», ветчина, салями, грибы.
+                </div>
+              </div>
+              <div class="card-buttons">
+                <button class="button button-primary button-add-cart">
+                  <span class="button-card-text">В корзину</span>
+                  <img src="img/shopping-cart-white.svg" alt="" class="button-cart-image">
+                </button>
+                <strong class="card-price-bold">900 ₸</strong>
+              </div>
+            </div>
+  `);
+
+  cardsMenu.insertAdjacentElement('beforeend', card);
+
+}
+
+function openGoods(event) {
+  const target = event.target;
+
+  const restaurant = target.closest('.card-restaurant');
+  console.log(restaurant);
+
+  if (restaurant) {
+    cardsMenu.textContent = '';
+
+    contPromo.classList.add('hide');
+    contRestaurants.classList.add('hide');
+    menu.classList.remove('hide');
+
+    createMenuCard();
+    createMenuCard();
+    createMenuCard();
+    createMenuCard();
+    createMenuCard();
+    createMenuCard();
+    createMenuCard();
+    createMenuCard();
+  }
+
+}
+
+logo.addEventListener('click', event => {
+  contPromo.classList.remove('hide');
+  contRestaurants.classList.remove('hide');
+  menu.classList.add('hide');
+})
+
+btnCart.addEventListener('click', toggleModal);
+btnCancel.addEventListener('click', toggleModal);
+btnClose.addEventListener('click', toggleModal);
+cardsRestaurants.addEventListener('click', openGoods);
+
+createCard();
+createCard();
+createCard();
+createCard();
+createCard();
+createCard();
+new WOW().init();
