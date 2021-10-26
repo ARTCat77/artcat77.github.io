510b6ef901ec8f8034fb94fc22d528bafb23d37a
Andrey Lekontsev <art.division210@gmail.com>
2021-10-26 22:26:32 +0600

    adaptive and scripts

--- delivery-service/js/myscript.js
+++ delivery-service/js/myscript.js
@@ -1,2 +1,18 @@
-
-
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  loop: true,
+  // autoplay parameters
+  autoplay: {
+    delay: 5000,
+  },
+  // effect
+  effect: "fade",
+  fadeEffect: {
+    crossFade: true,
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: ".slider-button-next",
+    prevEl: ".slider-button-prev",
+  },
+});
